b6317dc5358177557aa4f5d63ff2aa6f80356185
fishwang2016 <zhenyu2004@hotmail.com>
2016-08-13 23:23:53 +0800

    added deal

--- proker.py
+++ proker.py
@@ -12,9 +12,14 @@ import random # this will be a useful library for shuffling
 # on list comprehensions (you can find the link in the 
 # Instructor Comments box below).
 
-mydeck = [r+s for r in '23456789TJQKA' for s in 'SHDC'] 
 
-def deal(numhands, n=5, deck=mydeck):
+def deal(numhands, n=5, deck=[r+s for r in '23456789TJQKA' for s in 'SHDC'] ):
+    
+        # Your code here.
+    random.shuffle(deck)
+  
+    return [deck[n*i:n*(i+1)] for i in xrange(numhands)]
+            
     # Your code here.
 
 def poker(hands):
